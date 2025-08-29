23eb3b24c154e73830066457901514c883b9a76b
jupitters <sampaiobale@gmail.com>
2025-08-29 17:47:54 -0300

    fixes

--- src/context.jsx
+++ src/context.jsx
@@ -2,7 +2,7 @@ import { React, useContext } from 'react'
 
 const AppContext = React.createContext();
 
-const AppProvider = ({children}) => {
+const AppProvider = ({ children }) => {
   return <AppContext.Provider  value='hello'>
     {children}
   </AppContext.Provider>
