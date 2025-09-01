75cdbeb79ef8c2269f72280256bf0df5c30280f6
jupitters <sampaiobale@gmail.com>
2025-09-01 14:50:14 -0300

    cleaner response

--- src/context.jsx
+++ src/context.jsx
@@ -10,8 +10,8 @@ const AppProvider = ({ children }) => {
 
   const fetchMeals = async (url)=> {
       try {
-        const response = await axios(url);
-        console.log(response);
+        const {data} = await axios(url);
+        console.log(data);
       } catch (e) {
         console.log(e.response);
       }
