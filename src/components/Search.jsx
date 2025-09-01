caec2ba77f98d98ea3ab24a5c88f3a5482e2fefe
jupitters <sampaiobale@gmail.com>
2025-09-01 16:33:54 -0300

    handlers for submit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,10 +2,20 @@ import { useState } from "react"
 import useGlobalContext from "../GlobalContext"
 
 const Search = () => {
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <header className="search-container">
-      <form>
-        <input type="text" placeholder="Type your favorite meal!" className="form-input"/>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Type your favorite meal!" className="form-input" onChange={handleChange} value={text}/>
         <button type="submit" className="btn">search</button>
         <button type="button" className="btn btn-hipster">surprise me!</button>
       </form>
