c66fd9a7ace66e09d94456662feea19c19c28e3c
jupitters <sampaiobale@gmail.com>
2025-09-02 01:01:19 -0300

    modal view

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,21 @@
 import useGlobalContext from "../GlobalContext"
 
 const Modal = () => {
+  const { selectedMeal, closeModal } = useGlobalContext();
+
+  const { strMealThumb: image, strMeal: title, strInstructions: text, strSource: source } = selectedMeal
+
   return (
     <aside className="modal-overlay">
       <div className="modal-container">
-        <h1>modal container</h1>
+        <img src={image} className="img modal-img" />
+        <div className="modal-content">
+          <h4>{title}</h4>
+          <p>Cooking Instructions</p>
+          <p> {text} </p>
+          <a href={source} target="_blank">Original Source</a>
+          <button className="btn btn-hipster close-btn" onClick={closeModal}>close</button>
+        </div>
       </div>
     </aside>
   )
