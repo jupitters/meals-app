4d1e90162e7c7591938d9ab2f5a2b11d5d2508ff
jupitters <sampaiobale@gmail.com>
2025-09-02 01:31:10 -0300

    closing icon?

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import useGlobalContext from "../GlobalContext"
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 const Modal = () => {
   const { selectedMeal, closeModal } = useGlobalContext();
@@ -8,6 +9,7 @@ const Modal = () => {
   return (
     <aside className="modal-overlay">
       <div className="modal-container">
+        <IoIosCloseCircleOutline className="close-icon" onClick={closeModal}/>
         <img src={image} className="img modal-img" />
         <div className="modal-content">
           <h4>{title}</h4>
